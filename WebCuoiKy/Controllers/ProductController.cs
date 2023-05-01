3c97899be1dd7a935af1e19a843566fcb61dec1a
phannin <phannin91@gmail.com>
2023-05-01 20:53:08 +0700

    Add project files.

---
+++ WebCuoiKy/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WebCuoiKy.Models;
+
+namespace WebCuoiKy.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly dbMarketsContext _context;
+        public ProductController(dbMarketsContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+            if (product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
